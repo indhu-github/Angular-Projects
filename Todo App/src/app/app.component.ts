15e4c6e1e34ad429729545c4ac850763947c1e00
indhu-github <indhusworkmail@gmail.com>
2020-08-16 17:13:07 +0530

    Todo Application

---
+++ Todo App/src/app/app.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import {Todo} from './todo';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'first-todo';
+
+
+  todoValue:String;
+  list:Todo[];
+
+  ngOnInit(){
+    this.list=[];
+    this.todoValue="";
+  }
+
+  addItem(){
+    if(this.todoValue!==""){
+      const newItem:Todo={
+        id:Date.now(),
+        value:this.todoValue,
+        isDone:false
+      }
+      this.list.push(newItem);
+      console.log(this.list);
+    }
+    this.todoValue="";
+
+  }
+
+  deleteItem(id:number){
+    this.list=this.list.filter(item=>item.id!==id);
+  }
+}
